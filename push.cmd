a47328373d150b66c9fdc165af071885f08fb836
zmjack <jack@zmland.com>
2022-10-07 10:06:47 +0800

    v0.0.1

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
 nuget push "Prepare/bin/Release/Prepare.0.0.1.nupkg" -source nuget.org
-nuget push "Prepare.Cli/bin/Release/dotnet-prepare.0.0.1.9.nupkg" -source nuget.org
+nuget push "Prepare.Cli/bin/Release/dotnet-prepare.0.0.1.10.nupkg" -source nuget.org
 pause
